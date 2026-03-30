063372b02b1f34be6a01269b5151796497425103
digitalcoursed <m.pahomov.2002@mail.ru>
2026-03-30 20:47:09 +0300

    исправлен линкер, добавлен trampoline.S так как линкер без него выдает ошибку

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ CFLAGS="-std=gnu11 -O2 -g3 -Wall -Wextra --target=riscv64 -ffreestanding -nostdl
 
 # Сборка ядра
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
-  main.c common.c HAL.c kalloc.c vm.c entry.S
+  main.c common.c HAL.c kalloc.c vm.c entry.S trampoline.S
 
 # Запуск QEMU
 $QEMU -machine virt -bios default -nographic \
