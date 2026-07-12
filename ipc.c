04666cc922cf4904f0058a2b3ec3579a4adf2947
digitalcoursed <m.pahomov.2002@mail.ru>
2026-07-12 10:36:07 +0300

    добавлена передача данных ipc через регистры в заголовок send

--- ipc.c
+++ ipc.c
@@ -12,7 +12,12 @@
 #include "common.h"
 #include "ipc.h"
 
-int send(){
+int send(uint ID_dest,long arg0,long arg1,long arg2,long arg3,long arg7){
+  register long a0 __asm__("a0")=arg0;
+  register long a1 __asm__("a1")=arg1;
+  register long a2 __asm__("a2")=arg2;
+  register long a3 __asm__("a3")=arg3;
+  register long a7 __asm__("a7")=arg7;
   
 }
 
