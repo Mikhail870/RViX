34eba858165f75f3f3645dbb5c5c37f02d9fab7c
digitalcoursed <m.pahomov.2002@mail.ru>
2026-09-06 19:04:48 +0300

    исправлено имя сервера терминала с 10 на 11

--- main.c
+++ main.c
@@ -23,7 +23,7 @@ kinit(); // нарезка свободной памяти на стриницы
 kvminit(); // создание таблицы страниц ядра (требует изменения в vm.c !)
 kvminithart(); // включаем таблиццу страниц
 //intr_off(); 
-proc_born((uint64*)user_terminal_bin,(uint64)user_terminal_bin_len,10,1);// создание сервреа из бинарника
+proc_born((uint64*)user_terminal_bin,(uint64)user_terminal_bin_len,11,1);// создание сервреа из бинарника
 proc_born((uint64*)user_VFS_bin,(uint64)user_VFS_bin_len,10,0);// создание сервреа из бинарника
 proc_born((uint64*)user_prg3_bin,(uint64)user_prg3_bin_len,3,0);// создание процесса из бинарника
 proc_born((uint64*)user_prg2_bin,(uint64)user_prg2_bin_len,2,0);// создание процесса из бинарника
