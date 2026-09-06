ed1cdf282332a9a94110eeb557871a6e0da71596
digitalcoursed <m.pahomov.2002@mail.ru>
2026-09-06 11:02:31 +0300

    код отформатирован для читаемости в VFS.c

--- user/VFS.c
+++ user/VFS.c
@@ -6,25 +6,27 @@
 struct msg ipc_send;
 void main(void){
   while (1) {
-  ipc_send=recv();
-   // проверка номера вызова
-  switch (ipc_send.a3){
-    case 1: 
-    //проврека дескриптора
-  switch (ipc_send.a0) {
-    //write
-    case 1:
-  // перенаправление в консоль
-  break;
-    case 2:
-  // поток ошибок
-  break;
-  case 3:
-  // работа с файлом (перенаправление к фс)
-  break;
+    ipc_send=recv();
+    // проверка номера вызова
+    switch (ipc_send.a3){
+      case 1: 
+        //проврека дескриптора
+        switch (ipc_send.a0) {
+          //write
+          case 1:
+            // перенаправление в консоль
+            break;
+          case 2:
+            // поток ошибок
+            break;
+          case 3:
+            // работа с файлом (перенаправление к фс)
+            break;
         }
-  break;
-  case 2:
-  // open
+        break;
+      case 2:
+        // open
+        break;
+    }
   }
-}
+
