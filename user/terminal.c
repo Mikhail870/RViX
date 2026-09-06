db71d52627c6b3cf7c05fd1b0a55d3f2478ce1aa
digitalcoursed <m.pahomov.2002@mail.ru>
2026-09-06 22:12:53 +0300

    добавлна обработка \n в сервер консоли

--- user/terminal.c
+++ user/terminal.c
@@ -10,8 +10,13 @@ void main(){
   volatile char *uart=(char*)UART;
   while(1){
     ipc_send=recv();
+    char ch= (char)ipc_send.a1;
+    if (ch=='\n'){
+      *uart='\r';
+      *uart='\n';
+    } else {
     *uart=ipc_send.a1;
-
+    }
   }
   
 }
