c227b626e96a0993ee9f6a92a567d0a09e8e54f4
digitalcoursed <m.pahomov.2002@mail.ru>
2026-09-06 12:29:56 +0300

    добавлен маппинг страницы IPC в виртуальную память процесса по фиксированному адресу

--- process_manager.c
+++ process_manager.c
@@ -105,6 +105,12 @@ extern char trampoline[],uservec[],userret[];
     uvmunmap(npagetable, TRAMPOLINE, 1, 0);
     uvmfree(npagetable, 0);
   }
+  // маппинг буфера IPC на виртуальную память
+  if (mappages(npagetable, IPC, PGSIZE, (uint64)(pc->ipc_page),
+               PTE_R | PTE_W | PTE_U) < 0) {
+    uvmunmap(npagetable, TRAMPOLINE, 1, 0);
+    uvmfree(npagetable, 0);
+  }
   pc->pagetable=npagetable;
 
   // выделяем физическую страницу и мапим на 
