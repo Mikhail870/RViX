2eb70564e79c35eb0792ac5c41b21ff14111c1bb
digitalcoursed <m.pahomov.2002@mail.ru>
2026-09-06 11:04:47 +0300

    убран маппинг IPC страницы в таблицу страниц ядра

--- vm.c
+++ vm.c
@@ -42,8 +42,6 @@ kvmmake(void)
 
   // allocate and map a kernel stack for each process.
   //proc_mapstacks(kpgtbl);
- //маппинг IPC страницы 
-  kvmmap(kpgtbl, IPC , (uint64)IPC, PGSIZE, PTE_R | PTE_X);
   return kpgtbl;
 }
 
