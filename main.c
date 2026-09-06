d99e8d4359e2336944f6f11712dcd667acae5458
digitalcoursed <m.pahomov.2002@mail.ru>
2026-09-06 15:39:44 +0300

    добавлен запуск сервреа VFS в main.c

--- main.c
+++ main.c
@@ -13,7 +13,7 @@
 #include "user/prg1_data.h"
 #include "user/prg2_data.h"
 #include "user/prg3_data.h"
-
+#include "user/VFS_data.h"
 
 void  main() {
 printf("RViX booted !\n");
@@ -22,6 +22,7 @@ kinit(); // нарезка свободной памяти на стриницы
 kvminit(); // создание таблицы страниц ядра (требует изменения в vm.c !)
 kvminithart(); // включаем таблиццу страниц
 //intr_off(); 
+proc_born((uint64*)user_VFS_bin,(uint64)user_VFS_bin_len,10);// создание сервреа из бинарника
 proc_born((uint64*)user_prg3_bin,(uint64)user_prg3_bin_len,3);// создание процесса из бинарника
 proc_born((uint64*)user_prg2_bin,(uint64)user_prg2_bin_len,2);// создание процесса из бинарника
 proc_born((uint64*)user_prg1_bin,(uint64)user_prg1_bin_len,1);// создание процесса из бинарника
