94c9dfbf107dbc517b360dcbe28a55bc899096ce
digitalcoursed <m.pahomov.2002@mail.ru>
2026-09-16 08:54:30 +0300

    тестовое изменение, убран putchar

--- user/prg1.c
+++ user/prg1.c
@@ -5,7 +5,7 @@ putchar('1');
 puts("from IPC buffer !!");
 send(54321,8000,9000,9500,9700,9900,2);
 while(1){
-    putchar('1');
-    putchar('\n');
+    //putchar('1');
+   // putchar('\n');
   }
 }
