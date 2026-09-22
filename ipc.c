7836fb3f59428d26293b7d26988a725c9a429a00
digitalcoursed <m.pahomov.2002@mail.ru>
2026-09-22 11:06:37 +0300

    recv в регистре a7 возаращает имя отправителя

--- ipc.c
+++ ipc.c
@@ -74,7 +74,7 @@ void recv(void){
       PANIC("name source not found");
     }
     copy_reg(src,current);
-    current->trapframe->a5=name;
+    current->trapframe->a7=name;
     runable(src);
     src->ipc_data->is_wait_msg=0;
   } else {
