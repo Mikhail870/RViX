89480fe048f8c0576317d516fb3816a65d5019b8
digitalcoursed <m.pahomov.2002@mail.ru>
2026-09-24 21:39:53 +0300

    сервер терминала упрощен для вывода симола из сообщения IPC в регсистр UART

--- user/terminal.c
+++ user/terminal.c
@@ -7,18 +7,12 @@
 
 struct msg ipc_send;
 void main(){
-  volatile char *uart=(char*)UART;
+  volatile char *uart=(volatile char*)UART;
   char* strmem=(char*)IPC_BUFF;
   memset(strmem,0,PGSIZE);
 
   while(1){
     ipc_send=recv();
-    while(*strmem!=0){ 
-    *uart=*strmem;
-    strmem++;
-      if (*strmem==0)
-        break;
-    }
-    }
-  
+    *uart=ipc_send.a0;
+  } 
 }
