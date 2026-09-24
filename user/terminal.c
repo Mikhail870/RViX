b89ceee6a5d529967f9af7a49861f636158ee8f7
digitalcoursed <m.pahomov.2002@mail.ru>
2026-09-24 17:14:03 +0300

    отладка работы, страницу надо занулять для корректности вывода

--- user/terminal.c
+++ user/terminal.c
@@ -8,12 +8,19 @@
 struct msg ipc_send;
 void main(){
   volatile char *uart=(char*)UART;
+
+  char* strmem=(char*)IPC_BUFF;
+  strmem[0]='a';
+  strmem[1]=0;
+
   while(1){
-    ipc_send=recv();
+    //ipc_send=recv();
     char* strmem=(char*)IPC_BUFF;
-    while(!(*strmem==0)){ 
+    while(*strmem!=0){ 
     *uart=*strmem;
     strmem++;
+      if (*strmem==0)
+        break;
     }
     }
   
